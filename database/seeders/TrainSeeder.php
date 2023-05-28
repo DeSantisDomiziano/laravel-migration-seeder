1031d8e7a2b3fc76de75de33ed6a4f5b37b08309
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-29 00:14:45 +0200

    add faker param in trainseeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,8 +16,18 @@ class TrainSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 20; $i++) { 
-            
+        for ($i=0; $i < 25; $i++) { 
+            $train = new Train();
+            $train->azienda = $faker->firstName() . ' ' . $faker->lastName() . ' Company';
+            $train->stazione_di_partenza = 'Stazione ' . $faker->city() . ' Centro';
+            $train->stazione_di_arrivo = 'Stazione ' . $faker->city() . ' Centro';
+            $train->orario_di_partenza = $faker->date();
+            $train->orario_di_arrivo = $faker->date();
+            $train->codice_treno = $faker->bothify('??-#####');
+            $train->numero_carrozze = $faker->randomDigitNotNull();
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
         }
     }
 }
